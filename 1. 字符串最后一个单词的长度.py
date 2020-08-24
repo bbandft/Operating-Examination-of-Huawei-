7aa00a97f2d9b11db0dff12e3691c77fa8447b5a
saiz-lab <45285083+saiz-lab@users.noreply.github.com>
2020-08-25 06:06:23 +0900

    Update 1. 字符串最后一个单词的长度.py

--- 1. 字符串最后一个单词的长度.py
+++ 1. 字符串最后一个单词的长度.py
@@ -1,3 +1,4 @@
+# 方法一
 def word_len():
     i = input()
     if len(i)== 0 or len(i)>5000:
@@ -6,7 +7,7 @@ def word_len():
         return len(i.split(" ")[-1])
 print(word_len())
 
-#方法二
+# 方法二
 import sys
 i = sys.stdin.readline() #sys.stdin.readline( )会将标准输入全部获取，包括末尾的'\n'，input()会把‘\n’忽略
 print(len(i.split(" ")[-1])-1)
